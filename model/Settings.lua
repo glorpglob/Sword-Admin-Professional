dcdf125ad68fd134e6ea41d8cffe335d679a1cdc
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-04-21 01:11:15 +1200

    Create Settings.lua

---
+++ model/Settings.lua
@@ -0,0 +1,13 @@
+return { 
+	Terminal = true, --[[ 
+				use true for default, or insert your own custom MainModule ID to insert your own UI  
+					--]]
+	Admins = { 
+		1,
+		1,
+		1,
+	},
+	
+	
+	
+}
