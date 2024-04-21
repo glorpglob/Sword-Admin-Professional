a024c7f6c4f8125cf56b9501d7aa4f77707358cc
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-04-21 22:00:36 +1200

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -1,1 +1,25 @@
+local Parser = require(script.Parent.Parser)
 
+
+
+game.ReplicatedStorage:WaitForChild("Events_SAP").SendCommand.OnServerInvoke = function(Player, command) 
+	local CommandName = Parser.get_command(command) 
+	local output = "invalid"
+	
+	local Success, Error = pcall(function() 
+		local cur = require(script.Parent.CommandHandler)[CommandName]
+		output = cur[1](Player, 
+			{ 
+				cur["args"][1](command),
+				cur["args"][2](command),
+				cur["args"][3](command)
+			}
+		)
+	end)
+	
+	if not Success then 
+		output = "An error occured: " .. Error
+	end
+	
+	return output
+end
