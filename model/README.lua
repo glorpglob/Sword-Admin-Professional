219569d87a42c94de9daa15e2fa333a98fceca54
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-04-23 18:32:51 +1200

    Create README.lua

---
+++ model/README.lua
@@ -0,0 +1,44 @@
+--[[
+
+ .--.                        .-.   .--.    .-.          _       
+: .--'                       : :  : .; :   : :         :_;      
+`. `. .-..-..-. .--. .--.  .-' :  :    : .-' :,-.,-.,-..-.,-.,-.
+ _`, :: `; `; :' .; :: ..'' .; :  : :: :' .; :: ,. ,. :: :: ,. :
+`.__.'`.__.__.'`.__.':_;  `.__.'  :_;:_;`.__.':_;:_;:_;:_;:_;:_;
+											  for profressionals
+
+DIRECTIONS
+1. Adjust the Settings file
+2. Use RIGHT BRACKET or type ";open" in chat to open the GUI
+3. Use the GUI
+
+
+COPYRIGHT
+Licensed under the MIT license, this is OPEN SOURCE SOFTWARE
+available for all people to REDISTRIBUTE and SHARE. 
+
+
+LICENSE 
+MIT License
+
+Copyright (c) 2024 Sword Admin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+                                                                
+--]]
