744bcf59b792f5fe5fb41f74a3f7ac157c33bafe
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-04-23 18:28:51 +1200

    Update CommandHandler.lua

--- src/CommandHandler.lua
+++ src/CommandHandler.lua
@@ -1,4 +1,6 @@
 local Parser = require(script.Parent.Parser)
+local Data = require(script.Parent.Data)
+
 
 
 return  { 
@@ -25,17 +27,56 @@ return  {
 	}, 
 	["help"] = {
 		function(Player, args) 
-			game.ReplicatedStorage.Events_SAP.User.DirectOutput:FireClient(Player, [[
+			return [[
 			1. print 'input'
 			2. clear
 			3. help
-			]])
+			]]
 		end,
 		["args"] = {
 			function(command) end,
 			function(command) end,
 			function(command) end,
 		}
-	}, 
+	},  
+	["lua"] = {
+		function(Player, args) 
+			local output = loadstring(args[1])()
+			return "Code ran, check developer logs (f9)"
+		end,
+		["args"] = {
+			function(command) return Parser.find_in_bounds(command, "'") end,
+			function(command) end,
+			function(command) end,
+		}
+	},  
+	["rank1"] = {
+		function(Player, args) 
+			if not tonumber(args[1]) then 
+				return "Must be a user id"
+			end
+			table.insert(Data.RankScores.Rank1.Admins, tonumber(args[1]))
+			return "Success"
+		end,
+		["args"] = {
+			function(command) return string.split(command, " ")[2] end,
+			function(command) end,
+			function(command) end,
+		}
+	},  
+	["rank2"] = {
+		function(Player, args) 
+			if not tonumber(args[1]) then 
+				return "Must be a user id"
+			end
+			table.insert(Data.RankScores.Rank2.Admins, tonumber(args[1]))
+			return "Success"
+		end,
+		["args"] = {
+			function(command) return string.split(command, " ")[2] end,
+			function(command) end,
+			function(command) end,
+		}
+	}
 	
 }
