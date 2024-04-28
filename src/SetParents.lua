f2ebb6fda6bb04cb3b28cdad689ed7c4e6d71701
glorpglob <71954340+glorpglob@users.noreply.github.com>
2024-04-28 14:32:10 +1200

    Update SetParents.lua

--- src/SetParents.lua
+++ src/SetParents.lua
@@ -2,7 +2,3 @@ script.Parent.Name = "SAP"
 script.Parent.Parent = game.ServerScriptService
 script.Parent.Events_SAP.Parent = game.ReplicatedStorage
 script.Parent.SAP_Terminal.Parent = game.StarterGui
-
-game.Players.PlayerAdded:Connect(function(Player) 
-	--script.Parent.SAP_Terminal:Clone().Parent = Player.
-end)
